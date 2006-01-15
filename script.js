d3e5e2ffd566a141413d310a6a7344bac41e0bc9
greg <greg@35c55059-d0a5-da11-ade6-0050bffea3d9>
2006-01-15 22:42:00 +0000

    more features
    git-svn-id: file:///home/svn/quake-widget/trunk@9 35c55059-d0a5-da11-ade6-0050bffea3d9

--- script.js
+++ script.js
@@ -1,73 +1,98 @@
-/*
-var url = new URL();
-quakes = url.fetch("http://neic.usgs.gov/neis/finger/quake.asc");
-a = quakes.split("\n");
-for (var i = 0; i < a.length; i++) {
-    if (a[i][0] == '0') {
-        var lat = a[i].substring(19, 5);
-        if (a[i].charAt(24) == 'S') {
-            lat = -lat;
-        }
-        var lon = a[i].substring(26, 6);
-        if (a[i].charAt(32) == 'W') {
-            lon = -lon;
-        }
-        var mag = a[i].substring(40, 3);
-        var place = a[i].substring(49);
-        print(lat,lon,mag,place);
-    }
-}
-*/
-
-/*
-var url = new URL();
-url.outputFile = system.widgetDataFolder+"/quakes.xml.gz";
-url.fetch("http://earthquake.usgs.gov/recenteqsww/catalogs/merged_catalog.xml.gz");
-xml = runCommand("gunzip -c \""+url.outputFile+"\"");
-doc = XMLDOM.parse(xml);
-events = doc.documentElement.getElementsByTagName("event");
-for (var i = 0; i < events.length; i++) {
-    var lat = events.item(i).evaluate("string(param[@name='latitude']/@value)");
-    var lon = events.item(i).evaluate("string(param[@name='longitude']/@value)");
-    print(lat, lon);
-}
-*/
+var quakes = [];
+var minLoadedMagnitude;
+var zoom = 1;
+var zoomX, zoomY;
 
 function resetMap()
 {
-    if (preferences.mapType.value == "ringoffire") {
-        Map.visible = false;
-        MapE.visible = true;
-        MapW.visible = true;
+    if (zoom == 1) {
+        if (preferences.mapType.value == "ringoffire") {
+            Map.src = "Resources/world.jpg";
+            Map.width = Map.srcWidth;
+            Map.height = Map.srcHeight;
+            Map.hOffset = -150;
+            Map.vOffset = 0;
+            Map.visible = true;
+        } else {
+            Map.src = "Resources/world.jpg";
+            Map.width = Map.srcWidth;
+            Map.height = Map.srcHeight;
+            Map.hOffset = 0;
+            Map.vOffset = 0;
+            Map.visible = true;
+        }
     } else {
+        Map.src = "Resources/bigworld.jpg";
+        Map.width = 256 * zoom;
+        Map.height = 128 * zoom;
+        Map.hOffset = -zoomX/8*zoom + MapWindow.width/2;
+        Map.vOffset = -zoomY/8*zoom + MapWindow.height/2;
         Map.visible = true;
-        MapE.visible = false;
-        MapW.visible = false;
+    }
+    if (Map.hOffset > 0) {
+        Map2.src = Map.src;
+        Map2.width = Map.width;
+        Map2.height = Map.height;
+        Map2.hOffset = Map.hOffset - Map.width;
+        Map2.vOffset = Map.vOffset;
+        Map2.visible = true;
+    } else if (Map.hOffset + Map.width < MapWindow.width) {
+        Map2.src = Map.src;
+        Map2.width = Map.width;
+        Map2.height = Map.height;
+        Map2.hOffset = Map.hOffset + Map.width;
+        Map2.vOffset = Map.vOffset;
+        Map2.visible = true;
+    } else {
+        Map2.visible = false;
     }
     refresh();
 }
 
-var quakes = [];
-
-function clear()
+function clearDisplay()
 {
     for (var i = 0; i < quakes.length; ++i) {
-        quakes[i].circle.removeFromSuperview();
+        if (quakes[i].circle) {
+            quakes[i].circle.removeFromSuperview();
+        }
         if (quakes[i].label) {
             quakes[i].label.removeFromSuperview();
         }
     }
+}
+
+function clear()
+{
+    clearDisplay();
     quakes = [];
 }
 
 function reload()
 {
     print("reloading from earthquake.usgs.gov");
+    RetryTimer.ticking = false;
+    Error.visible = false;
+    Loading.visible = true;
+    updateNow();
     var req = new XMLHttpRequest();
-    req.open("GET", "http://earthquake.usgs.gov/recenteqsww/catalogs/eqs7day-M5.xml", false);
+    if (preferences.minMagnitude.value >= 50) {
+        req.open("GET", "http://earthquake.usgs.gov/recenteqsww/catalogs/eqs7day-M5.xml", false);
+        minLoadedMagnitude = 50;
+    } else {
+        req.open("GET", "http://earthquake.usgs.gov/recenteqsww/catalogs/eqs7day-M2.5.xml", false);
+        minLoadedMagnitude = 25;
+    }
     req.send();
+    Loading.visible = false;
+    updateNow();
     doc = XMLDOM.parse(req.responseText);
+    if (!doc.documentElement) {
+        RetryTimer.ticking = true;
+        Error.visible = true;
+        return;
+    }
     items = doc.documentElement.getElementsByTagName("item");
+    print("  "+items.length+" quakes loaded");
     if (items.length > 0) {
         clear();
         for (var i = 0; i < items.length; i++) {
@@ -87,20 +112,37 @@ function reload()
 
 function refresh()
 {
-    print("refresh");
+    print("refresh zoom="+zoom);
     var now = new Date();
+    clearDisplay();
     for (var i = 0; i < quakes.length; i++) {
         var q = quakes[i];
-        if (preferences.mapType.value == "ringoffire") {
-            mx = Map.width * ((q.lon + 180 + 200) % 360) / 360;
-        } else {
-            mx = Map.width * (q.lon + 180) / 360;
+        if (q.mag*10 < preferences.minMagnitude.value) {
+            continue;
         }
-        my = Map.height * (90 - q.lat) / 180;
+        if (zoom == 1) {
+            if (preferences.mapType.value == "ringoffire") {
+                mx = (MapWindow.width * (q.lon + 180) / 360 + 200) % MapWindow.width;
+            } else {
+                mx = MapWindow.width * (q.lon + 180) / 360;
+            }
+            my = MapWindow.height * (90 - q.lat) / 180;
+        } else {
+            mx = Map.width * (2048 * (q.lon + 180) / 360 - zoomX) / 2048 + MapWindow.width/2;
+            my = Map.height * (1024 * (90 - q.lat) / 180 - zoomY) / 1024 + MapWindow.height/2;
+        }
+        mx += MapWindow.hOffset;
+        my += MapWindow.vOffset;
         var menu = new Array();
         menu[0] = new MenuItem();
         menu[0].title = "Open quake info ("+q.title+")";
         menu[0].onSelect = "openURL(\""+q.link+"\")";
+        menu[1] = new MenuItem();
+        menu[1].title = "Zoom in";
+        menu[1].onSelect = "doZoom(zoom*2, "+(mx - MapWindow.hOffset)+", "+(my - MapWindow.vOffset)+")";
+        menu[2] = new MenuItem();
+        menu[2].title = "Zoom out";
+        menu[2].onSelect = "doZoom(1, 0, 0)";
         var c = new Image();
         if (now.getTime() - Date.parse(q.date) < 86400000) {
             c.src = "Resources/circle.png";
@@ -112,6 +154,9 @@ function refresh()
         c.hOffset = mx;
         c.vOffset = my;
         c.width = Math.exp(q.mag) / 30;
+        if (c.width < 5) {
+            c.width = 5;
+        }
         if (c.width > 70) {
             c.width = 70;
         }
@@ -119,30 +164,67 @@ function refresh()
         c.tooltip = q.title + "\n" + q.date;
         c.window = Main;
         c.contextMenuItems = menu;
-        var t = null;
+        var t = new Text();
         if (q.mag > 6.5) {
-            t = new Text();
             t.data = q.mag;
-            t.color = "#ffffff";
-            t.hAlign = "center";
-            t.hOffset = mx;
-            t.vOffset = my + 4;
-            t.size = 10;
-            t.style = "bold";
-            t.tooltip = q.title + "\n" + q.date;
-            t.window = Main;
-            t.contextMenuItems = menu;
+        } else {
+            t.data = "   ";
         }
-        if (q.circle) {
-            q.circle.removeFromSuperview();
-        }
-        if (q.label) {
-            q.label.removeFromSuperview();
-        }
+        t.color = "#ffffff";
+        t.hAlign = "center";
+        t.hOffset = mx;
+        t.vOffset = my + 4;
+        t.size = 10;
+        t.style = "bold";
+        t.tooltip = q.title + "\n" + q.date;
+        t.window = Main;
+        t.onMultiClick = menu[0].onSelect;
+        t.contextMenuItems = menu;
         q.circle = c;
         q.label = t;
+        if (zoom == 1) {
+            updateNow();
+        }
     }
 }
+
+function doZoom(newZoom, x, y)
+{
+    if (newZoom > 8) {
+        newZoom = 8;
+    }
+    if (zoom == 1) {
+        zoomX = 2048 * x / MapWindow.width;
+        zoomY = 1024 * y / MapWindow.height;
+        if (preferences.mapType.value == "ringoffire") {
+            zoomX = (zoomX + 2048 - 2048*(200/350)) % 2048;
+        }
+    } else {
+        zoomX += 8/zoom * (x - MapWindow.width/2)
+        zoomY += 8/zoom * (y - MapWindow.height/2)
+    }
+    zoom = newZoom;
+    resetMap();
+}
+
+function checkReload()
+{
+    if (preferences.minMagnitude.value < minLoadedMagnitude) {
+        reload();
+    } else {
+        resetMap();
+    }
+}
+
+var menu = new Array();
+menu[0] = new MenuItem();
+menu[0].title = "Zoom in";
+menu[0].onSelect = "doZoom(zoom*2, system.event.screenX - Main.hOffset - MapWindow.hOffset, system.event.screenY - Main.vOffset - MapWindow.vOffset)";
+menu[1] = new MenuItem();
+menu[1].title = "Zoom out";
+menu[1].onSelect = "doZoom(1, 0, 0)";
+Map.contextMenuItems = menu;
+Map2.contextMenuItems = menu;
 
 resetMap();
 updateNow();
